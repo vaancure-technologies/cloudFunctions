c3d653d99631afb0f5738a7af92da7843cfe006a
bairagiji <nishantgaur.nmims@gmail.com>
2020-02-14 19:17:44 +0530

    response json

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.addUserData = functions.https.onRequest(async (req, res) => {
+    const name = req.query.name;
+    // const snapshot = await admin.database().ref('/user').push({
+        const snapshot = await admin.database().ref('/user').push({
+        name : name
+    });
+    console.log(snapshot);
+    return res.JSON({
+        "value": snapshot,
+        "name": name
+    })
+});
